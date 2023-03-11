70a723452898ac65f591a71f1ec6217f9a3f6811
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-03-11 22:49:56 +0530

    Delete Report Subbmission directory

--- Report Subbmission/demo.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$FileName=$_FILES['fileUpload']['name'];
-	$TmpName=$_FILES['fileUpload']['tmp_name'];
-
-	move_uploaded_file($TmpName,$FileName);
-
-	echo("File Uploaded Successfully");
-?>
