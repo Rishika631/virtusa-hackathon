f6f7ff6be3c3d7981e5437c25719fd4d06cf1d9a
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-03-11 12:50:52 +0530

    Add files via upload

---
+++ request/client/src/App.js
@@ -0,0 +1,160 @@
+import "./App.css";
+//import React, { Component } from 'react';
+//import Modal from './modal';
+import { useState, useEffect } from "react";
+import Axios from "axios";
+
+function App() {
+  const [listOfemp, setListOfemp] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setMail] = useState("");
+  const [location, setLocation] = useState("");
+  const [duration, setDuration] = useState("");
+  const [date, setDate] = useState(new Date());
+  const [tech, setTech] = useState("");
+
+  
+
+  useEffect(() => {
+    Axios.get("http://localhost:8004/getemp").then((response) => {
+      setListOfemp(response.data);
+    });
+  }, []);
+
+  const createemp = () => {
+    Axios.post("http://localhost:8004/createemp", {
+      name,
+        email,
+        location,
+        duration,
+        date,
+        tech,
+     
+    }).then((response) => {
+      setListOfemp([
+        ...listOfemp,
+        {
+          name,
+          email,
+          location,
+          duration,
+          date,
+          tech,
+            
+        },
+      ]);
+    });
+  };
+
+  return (
+    <div className="app">
+    <form method="POST">
+          <div className="title">Job Shadowing Request</div>
+          
+          <div align="center"><div className="App"></div></div>
+
+            <div>
+              <label>
+              <div align="center">Name:</div>
+                <input
+          type="text"
+          
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+              </label>
+            </div>
+
+            <div>
+              <label>
+              <div align="center">Email:</div>
+                <input
+          type="text"
+          
+          onChange={(event) => {
+            setMail(event.target.value);
+          }}
+        />
+              </label>
+            </div>
+
+
+            <div>
+              <label>
+              <div align="center">Location:</div>
+                <input
+          type="text"
+          
+          onChange={(event) => {
+            setLocation(event.target.value);
+          }}
+        />
+              </label>
+            </div>
+
+            <div>
+              <label>
+              <div align="center">Duration:</div>
+                <input
+          type="text"
+          
+          onChange={(event) => {
+            setDuration(event.target.value);
+          }}
+        />
+              </label>
+            </div>
+          
+            <div>
+              <label>
+
+            <div align="center"> :</div>
+
+            <div className="App">
+        <input
+          // className={`form__input  ${!fromDate && "form__input--incomplete"}`}
+          id="date"
+          name="date"
+          type="date"
+          autoComplete="off"
+          value={
+            date.getFullYear().toString() +
+            "-" +
+            (date.getMonth() + 1).toString().padStart(2, 0) +
+            "-" +
+            date.getDate().toString().padStart(2, 0)
+          }
+          onChange={(e) => {
+            setDate(new Date(e.target.value));
+          }}
+        />
+      </div>
+      </label>
+            </div>
+
+            <div>
+              <label>
+              <div align="center">Tech:</div>
+                <input
+          type="text"
+          
+          onChange={(event) => {
+            setTech(event.target.value);
+          }}
+        />
+              </label>
+            </div>
+
+            
+            
+               
+      <div align="center">  <button className="sb-btn" type="button" onClick={createemp}>SUBMIT</button> </div>         
+      </form>
+    </div>       
+        );
+       //run client npm start and server node index.js
+  } 
+  
+
+export default App;
---
+++ request/server/index.js
@@ -0,0 +1,64 @@
+
+//import aposToLexForm from "apos-to-lex-form";
+//import { WordTokenizer, SentimentAnalyzer, PorterStemmer } from "natural";
+//import SpellCorrector from "spelling-corrector";
+//import stopword from "stopword";
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const fbModel = require("./models/fb");
+
+
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+    "mongodb+srv://rishika:Rishika06@cluster0.lmfgunj.mongodb.net/?retryWrites=true&w=majority"
+    
+  );
+
+
+ app.get("/", (req, res) => {
+    fbModel.find({}, (err, result) => {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(result);
+      }
+    });
+  });
+  
+
+
+
+// for html 
+app.get("/", (req, res) => {
+    res.render('/index.html');
+  });
+
+
+app.post("/createemp", async (req, res) => {
+  const fb = req.body;
+  const newfb = new fbModel(fb);
+  const user=await newfb.save();
+  res.json(user);
+});
+
+app.listen(8004, () => {
+  console.log("SERVER RUNS PERFECTLY!");
+});
+
+/*front end part <button onClick={(event) => {
+          {createUser};
+          setScore(getSentiment(review));
+        }}>Create User </button>*/
+
+
+
+
+
+
+
